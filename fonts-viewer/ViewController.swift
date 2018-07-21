3126b9ffd2a186d08aadd959368ce0865273b2ba
Светлана Лобан <svetlanaloban@svetlanas-macbook-air.local>
2018-07-21 13:52:06 +0300

    Initial Commit

---
+++ fonts-viewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fonts-viewer
+//
+//  Created by Светлана Лобан on 7/21/18.
+//  Copyright © 2018 Sviatlana Loban. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
