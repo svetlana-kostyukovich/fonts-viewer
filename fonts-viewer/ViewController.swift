42ed8ce25d97059027ece6ba9292acccbba1dbed
Светлана Лобан <svetlanaloban@svetlanas-macbook-air.local>
2018-07-22 12:19:02 +0300

    Segue and the second view controller added

--- fonts-viewer/ViewController.swift
+++ fonts-viewer/ViewController.swift
@@ -8,18 +8,19 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     // MARK: - Properties
     fileprivate let reuseIdentifier = "FontDescriptionCell"
-
+    fileprivate let rowItems: CGFloat = 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         getListOfFonts()
-
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -39,6 +40,44 @@ class ViewController: UICollectionViewController {
     }
     
     
+    let blogSegueIdentifier = "ShowFontDetailsSegue"
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if  segue.identifier == blogSegueIdentifier,
+            let destination = segue.destination as? FontDetailsViewController,
+            let fontIndex = collectionView?.indexPathsForSelectedItems//   indexPathForSelectedRow?.row
+        {
+        }
+    }
+    
+    
+    // MARK: - UICollectionViewDelegateFlowLayout
+    /* func collectionView(_ collectionView: UICollectionView,
+     layout collectionViewLayout: UICollectionViewLayout,
+     sizeForItemAt indexPath: IndexPath) -> CGSize {
+     let paddingSpace = sectionInset.left * (rowItems + 1)
+     let availableWidth = view.frame.width - paddingSpace
+     let widthPerItem = availableWidth / rowItems
+     
+     return CGSize(width: widthPerItem, height: widthPerItem)
+     }
+     
+     
+     func collectionView(_ collectionView: UICollectionView,
+     layout collectionViewLayout: UICollectionViewLayout,
+     insetForSectionAt section: Int) -> UIEdgeInsets {
+     return sectionInset
+     }
+     
+     
+     func collectionView(_ collectionView: UICollectionView,
+     layout collectionViewLayout: UICollectionViewLayout,
+     minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+     return sectionInset.left
+     } */
+    
+    
     func getListOfFonts() {
         
         UIFont.familyNames.forEach({ familyName in
@@ -55,6 +94,6 @@ class ViewController: UICollectionViewController {
             
         })
     }
-
+    
 }
 
