904f04e45327b844720910455445a30e830a61f4
Светлана Лобан <svetlanaloban@svetlanas-macbook-air.local>
2018-07-22 10:15:11 +0300

    Added fonts list creation (Model data)

---
+++ fonts-viewer/FontData.swift
@@ -0,0 +1,17 @@
+//
+//  FontData.swift
+//  fonts-viewer
+//
+//  Created by Светлана Лобан on 7/21/18.
+//  Copyright © 2018 Sviatlana Loban. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct FontData {
+    let fontName: String
+    let fontWeight: String
+    
+
+}
--- fonts-viewer/ViewController.swift
+++ fonts-viewer/ViewController.swift
@@ -9,9 +9,14 @@
 import UIKit
 
 class ViewController: UICollectionViewController {
+    
+    // MARK: - Properties
+    fileprivate let reuseIdentifier = "FontDescriptionCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        getListOfFonts()
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -26,13 +31,30 @@ class ViewController: UICollectionViewController {
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         //TODO: refactor reuseIdentifier
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FontDescriptionCell", for: indexPath) as! FontCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! FontCell
         
         //TODO: refactor
         cell.backgroundColor = UIColor.blue
         return cell
     }
-
+    
+    
+    func getListOfFonts() {
+        
+        UIFont.familyNames.forEach({ familyName in
+            let fontNames = UIFont.fontNames(forFamilyName: familyName)
+            for fontExtended in fontNames {
+                let nameIndex = fontExtended.index(of: "-") ?? fontExtended.endIndex  //font name extracting
+                let fontName = String(fontExtended[..<nameIndex])  // making a String from a Substring
+                
+                let weightIndex = nameIndex != fontExtended.endIndex ? fontExtended.index(nameIndex, offsetBy: 1) : fontExtended.endIndex // Weight extracting if existing
+                
+                let fontWeight = String(fontExtended[weightIndex...])
+                print("fontName: \(fontName), fontWeight:\(fontWeight)")
+            }
+            
+        })
+    }
 
 }
 
